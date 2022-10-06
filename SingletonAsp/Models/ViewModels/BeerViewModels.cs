71aa8f6b0353a0c387721691ba783752b211aea7
Vicente Estrada <67867277+vicente100est@users.noreply.github.com>
2022-10-06 15:04:37 -0500

    No ASP

--- SingletonAsp/Models/ViewModels/BeerViewModels.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace DesignPatternsAsp.Models.ViewModels
-{
-    public class BeerViewModels
-    {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Style { get; set; }
-    }
-}
